0b44a541cb001cf8b4bca17c7e3eeb6eba6d35a5
Yussuf Khalil <dev@pp3345.net>
2013-12-23 12:31:59 +0100

    Don't treat peer-triggered hangup as error condition in LinuxPoll module

--- LinuxPoll/PancakeLinuxPoll.c
+++ LinuxPoll/PancakeLinuxPoll.c
@@ -283,7 +283,7 @@ static void PancakeLinuxPollWait() {
 		for(i = 0; i < numEvents; i++) {
 			PancakeSocket *sock = (PancakeSocket*) events[i].data.ptr;
 
-			if((events[i].events & EPOLLHUP) || (events[i].events & EPOLLRDHUP)) {
+			if((events[i].events & EPOLLHUP)) {
 				sock->onRemoteHangup(sock);
 				PancakeCheckHeap();
 				continue;
@@ -296,6 +296,11 @@ static void PancakeLinuxPollWait() {
 
 			if(events[i].events & EPOLLOUT) {
 				sock->onWrite(sock);
+				PancakeCheckHeap();
+			}
+
+			if(events[i].events & EPOLLRDHUP) {
+				sock->onRemoteHangup(sock);
 				PancakeCheckHeap();
 			}
 		}
