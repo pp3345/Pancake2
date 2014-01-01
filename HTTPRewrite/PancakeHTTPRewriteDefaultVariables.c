dfa0cbbe642914eed7362f71f1d2bca1889ad45c
Yussuf Khalil <dev@pp3345.net>
2014-01-01 22:45:25 +0100

    Added variable $HTTPAnswerCode

--- HTTPRewrite/PancakeHTTPRewriteDefaultVariables.c
+++ HTTPRewrite/PancakeHTTPRewriteDefaultVariables.c
@@ -3,6 +3,8 @@
 
 #ifdef PANCAKE_HTTP_REWRITE
 
+#include "PancakeLogger.h"
+
 static UByte PancakeHTTPRewriteGetHTTPKeepAlive(PancakeSocket *sock, PancakeHTTPRewriteVariable *var, PancakeHTTPRewriteValue *value) {
 	PancakeHTTPRequest *request = (PancakeHTTPRequest*) sock->data;
 
@@ -17,6 +19,25 @@ static UByte PancakeHTTPRewriteSetHTTPKeepAlive(PancakeSocket *sock, PancakeHTTP
 	return 1;
 }
 
+static UByte PancakeHTTPRewriteGetHTTPAnswerCode(PancakeSocket *sock, PancakeHTTPRewriteVariable *var, PancakeHTTPRewriteValue *value) {
+	PancakeHTTPRequest *request = (PancakeHTTPRequest*) sock->data;
+
+	value->intv = (Int32) request->answerCode;
+	return 1;
+}
+
+static UByte PancakeHTTPRewriteSetHTTPAnswerCode(PancakeSocket *sock, PancakeHTTPRewriteVariable *var, PancakeHTTPRewriteValue *value) {
+	PancakeHTTPRequest *request = (PancakeHTTPRequest*) sock->data;
+
+	if(UNEXPECTED(value->intv < 100 || value->intv > 599)) {
+		PancakeLoggerFormat(PANCAKE_LOGGER_ERROR, 0, "Value for $HTTPAnswerCode is out of range (100 - 599)");
+		return 0;
+	}
+
+	request->answerCode = (UInt16) value->intv;
+	return 1;
+}
+
 static Byte PancakeHTTPRewriteThrowException(PancakeSocket *sock) {
 	PancakeHTTPRequest *request = (PancakeHTTPRequest*) sock->data;
 
@@ -28,6 +49,7 @@ static Byte PancakeHTTPRewriteThrowException(PancakeSocket *sock) {
 
 void PancakeHTTPRewriteRegisterDefaultVariables() {
 	PancakeHTTPRewriteRegisterVariable(StaticString("$HTTPKeepAlive"), PANCAKE_HTTP_REWRITE_BOOL, 0, NULL, PancakeHTTPRewriteGetHTTPKeepAlive, PancakeHTTPRewriteSetHTTPKeepAlive);
+	PancakeHTTPRewriteRegisterVariable(StaticString("$HTTPAnswerCode"), PANCAKE_HTTP_REWRITE_INT, 0, NULL, PancakeHTTPRewriteGetHTTPAnswerCode, PancakeHTTPRewriteSetHTTPAnswerCode);
 
 	PancakeHTTPRewriteRegisterCallback(StaticString("ThrowException"), PancakeHTTPRewriteThrowException);
 }
