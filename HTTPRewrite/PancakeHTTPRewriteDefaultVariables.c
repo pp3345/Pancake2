cba3e623916fe42b8fab0de4fc9e0a35964af94c
Yussuf Khalil <dev@pp3345.net>
2014-01-01 22:36:57 +0100

    Implemented ThrowException() rewrite callback

--- HTTPRewrite/PancakeHTTPRewriteDefaultVariables.c
+++ HTTPRewrite/PancakeHTTPRewriteDefaultVariables.c
@@ -17,8 +17,19 @@ static UByte PancakeHTTPRewriteSetHTTPKeepAlive(PancakeSocket *sock, PancakeHTTP
 	return 1;
 }
 
+static Byte PancakeHTTPRewriteThrowException(PancakeSocket *sock) {
+	PancakeHTTPRequest *request = (PancakeHTTPRequest*) sock->data;
+
+	PancakeHTTPException(sock, request->answerCode >= 100 && request->answerCode <= 599 ? request->answerCode : 500);
+
+	/* Stop parser */
+	return -1;
+}
+
 void PancakeHTTPRewriteRegisterDefaultVariables() {
 	PancakeHTTPRewriteRegisterVariable(StaticString("$HTTPKeepAlive"), PANCAKE_HTTP_REWRITE_BOOL, 0, NULL, PancakeHTTPRewriteGetHTTPKeepAlive, PancakeHTTPRewriteSetHTTPKeepAlive);
+
+	PancakeHTTPRewriteRegisterCallback(StaticString("ThrowException"), PancakeHTTPRewriteThrowException);
 }
 
 #endif
